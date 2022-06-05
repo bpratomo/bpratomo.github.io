6ab2abcc645f55442ca1e8c0c80d8c2aa13ede1e
Budi Pratomo <bpratomo@myrsm.nl>
2022-06-05 18:44:14 +0200

    added portfolio

--- Gemfile
+++ Gemfile
@@ -2,6 +2,7 @@
 source "https://rubygems.org"
 
 gem "github-pages", group: :jekyll_plugins
+gem "webrick"
 
 gem "tzinfo-data"
 gem "wdm", "~> 0.1.0" if Gem.win_platform?
---
+++ _site/assets/js/lunr/lunr-store.js
@@ -0,0 +1,7 @@
+var store = [{
+        "title": "Writing Jekyll Posts",
+        "excerpt":"testing just a default page  ","categories": [],
+        "tags": [],
+        "url": "/writing-jekyll-posts/",
+        "teaser": null
+      }]
